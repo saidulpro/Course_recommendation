6d3b6f4cafa13a964e475781d82b94e39e2c4d23
Saidul <sornalyakter@gmail.com>
2021-01-29 12:07:12 +0100

    Create Classifier.py

---
+++ code/Classifier.py
@@ -0,0 +1,64 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+df_dataset = pd.read_excel("C:\\Users\\Kmahmoud\\Desktop\\Course-Recommendation-System\\code\\encoded_data.xlsx")
+
+# Spliting data into test and train
+feature_names = df_dataset.columns.drop(['Student ID', 'Attepmpts'])
+X = df_dataset[feature_names]
+y = df_dataset['Course ID']
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+print(X_train, X_test, y_train, y_test)
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Decition Tree
+clf = DecisionTreeClassifier().fit(X_train, y_train)
+print('Accuracy of Decision Tree classifier on training set: {:.2f}'
+      .format(clf.score(X_train, y_train)))
+print('Accuracy of Decision Tree classifier on test set: {:.2f}'
+      .format(clf.score(X_test, y_test)))
+
+# Linear Discriminant Analysis
+lda = LinearDiscriminantAnalysis()
+lda.fit(X_train, y_train)
+print('Accuracy of LDA classifier on training set: {:.2f}'
+      .format(lda.score(X_train, y_train)))
+print('Accuracy of LDA classifier on test set: {:.2f}'
+      .format(lda.score(X_test, y_test)))
+
+# Gaussian Naive Bayes
+gnb = GaussianNB()
+gnb.fit(X_train, y_train)
+print('Accuracy of GNB classifier on training set: {:.2f}'
+      .format(gnb.score(X_train, y_train)))
+print('Accuracy of GNB classifier on test set: {:.2f}'
+      .format(gnb.score(X_test, y_test)))
+
+# Support Vector Machine
+svm = SVC()
+svm.fit(X_train, y_train)
+print('Accuracy of SVM classifier on training set: {:.2f}'
+      .format(svm.score(X_train, y_train)))
+print('Accuracy of SVM classifier on test set: {:.2f}'
+      .format(svm.score(X_test, y_test)))
+
+knn = KNeighborsClassifier()
+knn.fit(X_train, y_train)
+print('Accuracy of K-NN classifier on training set: {:.2f}'
+      .format(knn.score(X_train, y_train)))
+print('Accuracy of K-NN classifier on test set: {:.2f}'
+      .format(knn.score(X_test, y_test)))
+
+pred = knn.predict(X_test)
+print(confusion_matrix(y_test, pred))
+print(classification_report(y_test, pred))
